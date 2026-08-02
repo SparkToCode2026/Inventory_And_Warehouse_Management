194efaa82d8bb1a84cb5095d06a1879b9e710443
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-08-02 19:01:34 +0400

    add Project Context

---
+++ Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/ProjectContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory_And_Warehouse_Management
+{
+    internal class ProjectContext : DbContext
+    {
+        //1- register models
+
+
+
+        //2- connect to database
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(
+            "Server=.;Database=Inventory_And_Warehouse_Management_DB;Trusted_Connection=True;TrustServerCertificate=True;"
+            );
+        }
+    }
+}
