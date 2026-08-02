1adf44531ca774078a32d459d9d8dfba7213d6a6
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-08-02 18:49:34 +0400

    add project files

---
+++ Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Inventory_And_Warehouse_Management
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
