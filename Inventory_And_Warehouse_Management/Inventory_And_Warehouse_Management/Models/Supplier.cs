e82ff17dbea062aec34e84e2cbef722bffcc6e4a
Issa Al Sibani <issaalsibani12@gmail.com>
2026-08-04 17:02:54 +0400

    Create Supplier and Customer models

---
+++ Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/Models/Supplier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Inventory_And_Warehouse_Management.Models
+{
+    public class Supplier
+    {
+        [Key]
+        public int SupplierId { get; set; }
+        public string Name { get; set; }
+        
+        public string Phone { get; set; }
+        public string Email { get; set; }
+     
+    }
+}
