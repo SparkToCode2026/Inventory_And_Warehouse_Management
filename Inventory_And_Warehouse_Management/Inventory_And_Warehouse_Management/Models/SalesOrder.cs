aaa99c7933079a08edf3a1e312a50a8bd66ea790
zuwaina alrashdi <zuwainaalrashdi2005@gmail.com>
2026-08-04 15:25:16 +0400

    Add SalesOrder & SalesOrderItem Classes

---
+++ Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/Models/SalesOrder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory_And_Warehouse_Management.Models
+{
+    public class SalesOrder
+    {
+        [Key]
+        public int SalesOrderId { get; set; }
+
+        public string Status { get; set; }
+
+        public decimal TotalAmount { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public int UserId { get; set; }
+
+        
+        [ForeignKey("CustomerId")]
+        public Customer Customer { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
+        public List<SalesOrderItem> SalesOrderItems { get; set; }
+    }
+}
---
+++ Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/Models/SalesOrderItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory_And_Warehouse_Management.Models
+{
+    public class SalesOrderItem
+    {
+        [Key]
+        public int SalesOrderItemId { get; set; }
+
+        public int SalesOrderId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+        
+        [ForeignKey("SalesOrderId")]
+        public SalesOrder SalesOrder { get; set; }
+
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+    }
+}
