7bc06818a23f29c090e15c84f040589b39512a52
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-08-05 13:10:17 +0400

    the full Category ↔ Product relationship

--- Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/Models/Category.cs
+++ Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/Models/Category.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Inventory_And_Warehouse_Management.Models
@@ -11,5 +12,10 @@ namespace Inventory_And_Warehouse_Management.Models
         public int CategoryId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+
+
+        //product has Category
+        [InverseProperty("category")]
+        public List<Product> products { get; set; }
     }
 }
--- Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/Models/Product.cs
+++ Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/Models/Product.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Inventory_And_Warehouse_Management.Models
@@ -14,8 +15,10 @@ namespace Inventory_And_Warehouse_Management.Models
         public string Description { get; set; }
 
 
-        //Belongs to 
+        //product has Category
+        [ForeignKey("category")]
         public int CategoryId { get; set; }
+        public Category category { get; set; }
     }
 
 }
