cbc7790423ad8d68b627ad3c6995a47c6086c910
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-08-05 22:17:46 +0400

    add cases: Create a new Category Delete a Category Create a new Product Delete a Product

---
+++ Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+﻿using Inventory_And_Warehouse_Management.Models;
+
+namespace Inventory_And_Warehouse_Management.Controllers
+{
+    public class CategoryController
+    {
+        private ProjectContext context;
+
+        public CategoryController(ProjectContext _context)
+        {
+            context = _context;
+        }
+
+
+        //Create a new Category
+        public void AddCategory(Category c)
+        {
+            context.categories.Add(c);
+            context.SaveChanges();
+        }
+
+
+        //Delete a Category
+        public void DeleteProduct(int id)
+        {
+            Category category = context.categories.FirstOrDefault(c => c.CategoryId == id);
+
+            if (category == null)
+            {
+
+            }
+            else
+            {
+                context.categories.Remove(category);
+                context.SaveChanges();
+            }
+        }
+
+
+    }
+}
---
+++ Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using Inventory_And_Warehouse_Management.Models;
+
+namespace Inventory_And_Warehouse_Management.Controllers
+{
+    public class ProductController
+    {
+        private ProjectContext context;
+
+        public ProductController(ProjectContext _context)
+        {
+            context = _context;
+        }
+
+
+        //Create a new Product
+        public void AddProduct(Product p)
+        {
+            context.products.Add(p);
+            context.SaveChanges();
+        }
+
+
+        //Delete a Product
+        public void DeleteProduct(int id)
+        {
+            Product product = context.products.FirstOrDefault(p => p.ProductId == id);
+
+            if (product == null)
+            {
+
+            }
+            else
+            {
+                context.products.Remove(product);
+                context.SaveChanges();
+            }
+        }
+
+
+    }
+}
