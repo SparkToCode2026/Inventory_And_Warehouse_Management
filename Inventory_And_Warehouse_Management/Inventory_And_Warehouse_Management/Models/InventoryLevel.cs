507a588a8a6619bf94aecbee883c29165e6a1a5d
Reham <rehamxb@gmail.com>
2026-08-05 11:02:09 +0400

    comment on InventoryLevel Class

--- Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/Models/InventoryLevel.cs
+++ Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/Models/InventoryLevel.cs
@@ -14,12 +14,16 @@ namespace Inventory_And_Warehouse_Management.Models
 
         [ForeignKey(nameof(Warehouse))]
         public int WarehouseId { get; set; }
+
+        // Link back to Warehouse.
         public Warehouse Warehouse { get; set; }
 
 
 
         [ForeignKey(nameof(Product))]
         public int ProductId { get; set; }
+
+        // Link back to Product.
         public Product Product { get; set;}
 
         public int QuantityOnHand { get; set; }
