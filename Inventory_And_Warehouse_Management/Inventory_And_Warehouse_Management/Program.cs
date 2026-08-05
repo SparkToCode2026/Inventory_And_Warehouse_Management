da2d079eaf6fed2760eaa7317b8f87b530e42b8d
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-08-05 17:45:06 +0400

    update wep api files

--- Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/Program.cs
+++ Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/Program.cs
@@ -1,19 +1,31 @@
-﻿var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+﻿namespace Inventory_And_Warehouse_Management.Models
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-
-app.Run();
