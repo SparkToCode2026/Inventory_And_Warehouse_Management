0a1b18c99300fac03098c09334b6869e631bd657
Reham <rehamxb@gmail.com>
2026-08-05 09:53:54 +0400

    Add Relation in InventoryLevel

--- Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/Models/InventoryLevel.cs
+++ Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/Models/InventoryLevel.cs
@@ -11,11 +11,16 @@ namespace Inventory_And_Warehouse_Management.Models
         [Key]
         public int InventoryLevelId { get; set; }
 
+
         [ForeignKey(nameof(Warehouse))]
         public int WarehouseId { get; set; }
+        public Warehouse Warehouse { get; set; }
+
+
 
         [ForeignKey(nameof(Product))]
         public int ProductId { get; set; }
+        public Product Product { get; set;}
 
         public int QuantityOnHand { get; set; }
         public int ReorderThreshold { get; set; }
