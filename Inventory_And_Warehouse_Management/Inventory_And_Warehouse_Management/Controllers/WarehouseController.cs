4460c7449bb67fb685caa718a182a41af3cadf51
Reham <rehamxb@gmail.com>
2026-08-06 13:33:17 +0400

    Case2:Update a Warehouse (full update

--- Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/Controllers/WarehouseController.cs
+++ Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/Controllers/WarehouseController.cs
@@ -22,5 +22,25 @@ namespace Inventory_And_Warehouse_Management.Controllers
             context.warehouses.Add(w);
             context.SaveChanges();
         }
+
+        //Case2:Update a Warehouse (full update)
+        [HttpPut("UpdateWarehouse")]
+        public void UpdateWarehouse(int id, string name, int capacity, string location, string phone)
+        {
+            Warehouse warehouse = context.warehouses.FirstOrDefault(w => w.WarehouseId == id);
+
+            if (warehouse == null)
+            {
+
+            }
+            else
+            {
+                warehouse.Name = name;
+                warehouse.Capacity = capacity;
+                warehouse.Location = location;
+                warehouse.Phone = phone;
+                context.SaveChanges();
+            }
+        }
     }
 }
