a4edc46502ff63fb0deadfba6bc4c9ba5ad6359d
Reham <rehamxb@gmail.com>
2026-08-06 22:00:13 +0400

    WarehouseController Class : Case 2 Changed void and added return

--- Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/Controllers/WarehouseController.cs
+++ Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/Controllers/WarehouseController.cs
@@ -42,13 +42,13 @@ namespace Inventory_And_Warehouse_Management.Controllers
 
         //Case2:Update a Warehouse (full update)
         [HttpPut("UpdateWarehouse")]
-        public void UpdateWarehouse(int id, string name, int capacity, string location, string phone)
+        public IActionResult UpdateWarehouse(int id, string name, int capacity, string location, string phone)
         {
             Warehouse warehouse = context.warehouses.FirstOrDefault(w => w.WarehouseId == id);
 
             if (warehouse == null)
             {
-
+                return NotFound("Warehouse " + id + " was not found.");
             }
             else
             {
@@ -57,6 +57,8 @@ namespace Inventory_And_Warehouse_Management.Controllers
                 warehouse.Location = location;
                 warehouse.Phone = phone;
                 context.SaveChanges();
+
+                return Ok(warehouse);
             }
         }
 
