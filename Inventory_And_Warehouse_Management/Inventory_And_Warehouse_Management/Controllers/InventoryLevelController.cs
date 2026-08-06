40e889b7d7561c00fad4eb47dece1e20f7750802
Reham <rehamxb@gmail.com>
2026-08-06 20:54:07 +0400

    InevtoryLevelController: changed void to IActionResult

--- Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/Controllers/InventoryLevelController.cs
+++ Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/Controllers/InventoryLevelController.cs
@@ -55,7 +55,7 @@ namespace Inventory_And_Warehouse_Management.Controllers
 
         //Case2: Update an InventoryLevel (full update)
         [HttpPut("UpdateInventoryLevel")]
-        public void UpdateInventoryLevel(int warehouseId, int productId, int quantityOnHand, int reorderThreshold)
+        public IActionResult UpdateInventoryLevel(int warehouseId, int productId, int quantityOnHand, int reorderThreshold)
         {
             InventoryLevel inventoryLevel = Context.InventoryLevels.FirstOrDefault(il => il.WarehouseId == warehouseId && il.ProductId == productId);
 
@@ -74,7 +74,7 @@ namespace Inventory_And_Warehouse_Management.Controllers
 
         //Case3: A second distinct update case (adjust quantity up or down)
         [HttpPatch("AdjustQuantity")]
-        public void AdjustQuantity(int warehouseId, int productId, int delta)
+        public IActionResult AdjustQuantity(int warehouseId, int productId, int delta)
         {
             InventoryLevel inventoryLevel = Context.InventoryLevels.FirstOrDefault(il => il.WarehouseId == warehouseId && il.ProductId == productId);
 
@@ -93,7 +93,7 @@ namespace Inventory_And_Warehouse_Management.Controllers
 
         //Case4: Delete an InventoryLevel record
         [HttpDelete("DeleteInventoryLevel")]
-        public void DeleteInventoryLevel(int warehouseId, int productId)
+        public IActionResult DeleteInventoryLevel(int warehouseId, int productId)
         {
             InventoryLevel inventoryLevel = Context.InventoryLevels.FirstOrDefault(il => il.WarehouseId == warehouseId && il.ProductId == productId);
 
