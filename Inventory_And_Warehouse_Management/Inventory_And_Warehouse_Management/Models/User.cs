48d5119cc2609d278e43ab6f6fd272fa95215662
sara <sara.y.alhinai@gmail.com>
2026-08-06 16:24:17 +0400

    user model fixes

--- Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/Models/User.cs
+++ Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/Models/User.cs
@@ -10,20 +10,21 @@ namespace Inventory_And_Warehouse_Management.Models
     public class User
     {
         [Key]
+        [JsonIgnore]
         public int UserId { get; set; }
         [Required]
-        public string Name { get; set; }
+        public string? Name { get; set; }
         [Required]
 
-        public string Email { get; set; }
+        public string? Email { get; set; }
         [Required]
-        public string PasswordHash { get; set; }
-        [Required]
-
-        public string Role { get; set; }
+        public string? PasswordHash { get; set; }
         [Required]
 
-        public string Phone { get; set; }
+        public string? Role { get; set; }
+        [Required]
+
+        public string? Phone { get; set; }
 
         //Work
         [ForeignKey("_warehouse")]
