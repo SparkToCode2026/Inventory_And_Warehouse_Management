94f30478e0f100930568b26c52a14f6c2029da39
Abdullah Al Shamsi <ibnhilal2003@gmail.com>
2026-08-08 22:08:45 +0400

    Authorize PurchaseOrder & PurchaseOrderItem

--- Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/Controllers/PurchaseOrderItemController.cs
+++ Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/Controllers/PurchaseOrderItemController.cs
@@ -1,4 +1,5 @@
 ﻿using Inventory_And_Warehouse_Management.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,6 +7,7 @@ namespace Inventory_And_Warehouse_Management.Controllers
 {
     [ApiController]
     [Route("PurchaseOrderItem")]
+    [Authorize]
     public class PurchaseOrderItemController : ControllerBase
     {
         private ProjectContext context;
