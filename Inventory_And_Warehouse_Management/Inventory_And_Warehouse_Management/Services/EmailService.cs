a7f2878b3e056fc0ca312c1033f1e18a235d5803
sara <sara.y.alhinai@gmail.com>
2026-08-08 15:40:44 +0400

    EmailService classes

---
+++ Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/Services/EmailService.cs
@@ -0,0 +1,35 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+
+namespace Inventory_And_Warehouse_Management.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _config;
+
+        public EmailService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string body)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress(_config["Smtp:FromName"], _config["Smtp:FromEmail"]));
+            message.To.Add(MailboxAddress.Parse(toEmail));
+            message.Subject = subject;
+            message.Body = new TextPart("plain") { Text = body };
+
+            using var client = new SmtpClient();
+            await client.ConnectAsync(
+                _config["Smtp:Host"],
+                int.Parse(_config["Smtp:Port"]!),
+                SecureSocketOptions.StartTls);
+
+            await client.AuthenticateAsync(_config["Smtp:User"], _config["Smtp:Password"]);
+            await client.SendAsync(message);
+            await client.DisconnectAsync(true);
+        }
+    }
+}
