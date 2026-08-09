05477b6ac8ac7c614ce7bc2e9df76be668e0db5f
sara <sara.y.alhinai@gmail.com>
2026-08-09 10:49:33 +0400

    Remove Required From WarehouseId

---
+++ Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/Migrations/20260809064857_RemoveRequiredFromWarehouseId.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inventory_And_Warehouse_Management.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveRequiredFromWarehouseId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_users_warehouses_WarehouseId",
+                table: "users");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "WarehouseId",
+                table: "users",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_users_warehouses_WarehouseId",
+                table: "users",
+                column: "WarehouseId",
+                principalTable: "warehouses",
+                principalColumn: "WarehouseId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_users_warehouses_WarehouseId",
+                table: "users");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "WarehouseId",
+                table: "users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_users_warehouses_WarehouseId",
+                table: "users",
+                column: "WarehouseId",
+                principalTable: "warehouses",
+                principalColumn: "WarehouseId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
