44c488bf92131c6ff478c9be991831c51a03334e
Reham <rehamxb@gmail.com>
2026-08-10 20:22:29 +0400

    class (PurchaseOrderItem): add [Jsonlgnor] to use only IDs for product and purchaseOrder instead of full objects

--- Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/Models/PurchaseOrderItem.cs
+++ Inventory_And_Warehouse_Management/Inventory_And_Warehouse_Management/Models/PurchaseOrderItem.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace Inventory_And_Warehouse_Management.Models
 {
@@ -18,12 +19,14 @@ namespace Inventory_And_Warehouse_Management.Models
         //Relationship for PurchaseOrderItem and purchaseOrder
         [ForeignKey("purchaseOrder")]
         public int PurchaseOrderId { get; set; }
+        [JsonIgnore]
         public PurchaseOrder purchaseOrder { get; set; }
 
 
         //Relationship for PurchaseOrderItem and Product
         [ForeignKey("product")]
         public int ProductId { get; set; }
+        [JsonIgnore]
         public Product product { get; set; }
     }
 }
